78de830a070bd0c56f23c8f4f2e0c4bf006a3ac9
code123841 <udhay1co.de@gmail.com>
2022-05-15 18:41:23 +0530

    📦 Pkg Control Up in running Test Pass

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,2 @@
-#!/usr/bin/env node
-
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-
-yargs(hideBin(process.argv))
-  // Use the commands directory to scaffold.
-  .commandDir('commands')
-  // Enable strict mode.
-  .strict()
-  // Useful aliases.
-  .alias({ h: 'help' })
-  .argv;
+import {greet} from "./commands/greet";
+greet('uhd')
