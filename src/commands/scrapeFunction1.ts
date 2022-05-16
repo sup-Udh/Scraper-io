9cd8f884199106d2823b6f5920b85d1355731621
code123841 <udhay1co.de@gmail.com>
2022-05-16 16:10:08 +0530

    📦 NEW: Url Detection

--- src/commands/scrapeFunction1.ts
+++ src/commands/scrapeFunction1.ts
@@ -1,10 +1,13 @@
 export function scrapeFunction1(searchTerm : string){
     // check the url if the url is amazon.com then console.log the url
     // if the url is not amazon.com then console.log the url
-    if(searchTerm.includes("amazon")){
-        console.log('has amazon :) ')
+    var stringified = JSON.stringify(searchTerm);
+    var textified = stringified.toString();
+    if(textified.includes("amazon")){
+        console.log("yes amazon.com");
     }else{
-        console.log('no amazon')
+        console.log("not amazon.com");
     }
+
     
 }
