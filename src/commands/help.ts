de93e305b9004da8a204b8fb038b579b3a0daed3
code123841 <udhay1co.de@gmail.com>
2022-05-16 15:39:20 +0530

    📦 NEW: Packages + node src Updates

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -5,30 +5,23 @@ let path = require('path');
 let program = require('commander');
 
 export function help() {
-
-
-
 program
   .version('0.0.1 --Pre-Alpha')
   .description("Scrpe Your Favorite websites in minutes!")
+  
   .option('-s, --scrape ', 'Scrape Your Website')
   .option('-ds, --Defaultsites', 'Templates we have for you')
   .option('-h, --help', 'Help')
-
   .parse(process.argv);
 
 const options = program.opts();
 console.log(options);
 
-if (options.scrape) console.log('  Scrape!');
+if (options.scrape){
+
+}
 if (options.ds) console.log('ds');
 if(options.help) console.log("helping");
-
-// const cheese: string = undefined === options.cheese
-//     ? 'marble'
-//     : options.cheese || 'no';
-
-// console.log('  - %s cheese', cheese);
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
