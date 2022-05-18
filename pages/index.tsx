7450e499e4f27b68c6495b67cd0ed695b0dd8fd8
code123841 <udhay1co.de@gmail.com>
2022-05-18 13:31:10 +0530

    Breakpoints all working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,33 @@
+import Image from "next/image"
+import thumbnail from "../public/thubnail.png"
 export default function Home() {
   return (
     <body className="bg-[#141623]">
-     
+
 
       <div className="pl-10 pt-[15rem]">
-    <h1 className="text-[5rem] text-white font-inter font-bold">
-      Web Scraping <br /> 
-      simplified.
-    </h1>
-    <p className="text-[20px] text-[#A9A5A6] font-inter font-normal mt-[2rem]">
-    Focus on extracting the data you need,<br />
-     and not dealing with concurrent headless <br />
-     browsers that will eat up all your RAM and CPU.
-    </p>
-    
+        <h1 className="text-[5rem] text-white font-inter font-bold">
+          Web Scraping <br />
+          simplified.
+        </h1>
+        <p className="text-[20px] text-[#A9A5A6] font-inter font-normal mt-[2rem]">
+          Focus on extracting the data you need,<br />
+          and not dealing with concurrent headless <br />
+          browsers that will eat up all your RAM and CPU.
+        </p>
+
       </div>
 
-      <div className="flex justify-end pr-[10rem]">
-        <img src={require('../public/thubnail.png')} alt="scraper" />
-        <h1 className="mt-[-20rem] ">lol</h1>
+      <div className="flex justify-end pr-[5rem] lg:mt-[-28rem] md: mt-[10rem] ">
+        <Image
+        className="mt-[-10rem]"
+          src={thumbnail}
+          height={800}
+          width={900}
+        />
+
       </div>
     </body>
- 
+
   )
 }
