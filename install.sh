3d0df4df5a99768dc4b3831f3f3aab184c1b37b8
code123841 <udhay1co.de@gmail.com>
2022-05-19 17:07:44 +0530

    🐛 FIX: Npm modules

--- install.sh
+++ install.sh
@@ -2,7 +2,14 @@
 
 echo 'Installing.. the Required Modules'
 
+
+git clone  https://github.com/code123841/Scrapers.io/tree/cli-scraper
+
 # run npm install puppeteer
 
 npm install puppeteer -g
+npm install clear -g
+npm install commander -g
 
+
+echo 'Done And ready to use!'
