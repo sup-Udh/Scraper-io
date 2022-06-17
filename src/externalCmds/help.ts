759eb1ae298182c5318657b8f9b85fd06708e415
code123841 <udhay1co.de@gmail.com>
2022-06-17 11:08:33 +0530

    🔥 REMOVE: HELP

--- src/externalCmds/help.ts
+++ src/externalCmds/help.ts
@@ -1,3 +1,6 @@
 export function terminalhelp() {
-    console.log("help");
+    console.log("-s --scrape : Scrape your Websites 👀");
+    console.log("-ds --Defaultsites : Templates we have for you which are only avaliable for the CLI version 🔥");
+
+    console.log("NOTE: ALL THE FEATURES ARE NOT AVALIABLE FOR THE CLI VERSION ❌");
 }
