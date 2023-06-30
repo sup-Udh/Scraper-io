7501adf28491c92770e1409159032e8aca2b23c4
Udhay rajeev <udhay1co.de@gmail.com>
2023-06-30 20:26:27 +0530

    about pages

--- app/page.tsx
+++ app/page.tsx
@@ -33,25 +33,33 @@ export default function Home() {
     {/* Buttons content */}
 
     <div className='flex justify-center mt-[4.25rem]'>
-      <button className='border-[2px] border-[#fff] h-[36px] w-[239px] rounded-md font-light'>Sign Up</button>
+      <button className='border-[2px] border-[#fff] h-[36px] w-[239px] rounded-md font-light'>
+        <a href="#waitlist">Sign Up</a>
+        </button>
       <button className='border-1 bg-[#3C3E43] h-[36px] w-[239px] rounded-md font-thin ml-[1.25rem]'>Learn More</button>
     </div>
 
 
 {/* animation buttons */}
     <div className='flex justify-center mt-[3.25rem] animate-bounce'>
+     
+      <button>
+        <a href="#waitlist">
     <Image 
     src={arrow}
     height={30}
     alt='arrow'
     className='mt-[4.25rem]'
     />
+    </a>
+    </button>
 
 
     </div>
-  
+
+    <div id="waitlist">
     {/* div for the waitlist */}
-     <div className=" rounded-lg mt-[400px] w-full h-[20rem] bg-gradient-to-b from-purple-500 to-blue-500">
+     <div className=" rounded-lg mt-[400px] w-full h-[25rem] bg-gradient-to-b from-purple-500 to-blue-500">
       
       {/* title  */}
       
@@ -78,6 +86,8 @@ export default function Home() {
       <input placeholder='email' type='email' required className='text-left px-2 h-[40px] w-[243px] mt-[2.5rem] border-2 bg-[#fff] rounded-md  focus:border-sky-500 focus:ring-sky-500   text-black '/>
       {/* add a button parallel to the input area  */}
       <button className='mt-[2.5rem] ml-[1.25rem] bg-sky-500 rounded-md h-[4opx] w-[100px] text-sm font-medium'>Join Waitlist</button>
+    </div>
+
     </div>
 
 
