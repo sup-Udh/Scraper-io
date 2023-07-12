b7ab3c30db8119a634ec280adfe2b77793cafec1
Udhay rajeev <udhay1co.de@gmail.com>
2023-07-12 20:26:55 +0530

    Fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -121,7 +121,7 @@ export default function Home() {
 
       {/* Danger did not workout! */}
       <div id="container" className={hiddenfail ? 'hidden': ".hideMe p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"} role="alert">
-  <span className="font-medium">Danger alert!</span> Change a few things up and try submitting again.
+  <span className="font-medium">You've already been added</span> Change a few things up and try submitting again.
 </div>
       {/* alert box || sucess */}
      <div id="container" className={hiddensucess ? 'hidden' : ".hideMe p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"} role="alert">
