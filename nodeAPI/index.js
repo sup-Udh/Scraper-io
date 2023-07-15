0e48df92ce001611151f23192f0c034b22172655
Udhay rajeev <udhay1co.de@gmail.com>
2023-07-15 21:37:52 +0530

    express setup

---
+++ nodeAPI/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+}
+);
+
+app.listen(3001, () => {
+    console.log('Server is running at port 3001');
+})
