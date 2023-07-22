ddaac9d1876b4885c8dc226a86574bf6b5d7d958
Udhay rajeev <udhay1co.de@gmail.com>
2023-07-22 20:52:33 +0530

    Remove api

--- pages/user/scraper.tsx
+++ pages/user/scraper.tsx
@@ -1,6 +1,34 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
 export default function scraper(){
+    const [url , setUrl] = useState('')
+    const [elements , setElements] = useState('')
+
+    const datasend = async (e:any) => {
+        e.preventDefault();
+        try{
+            const response = await axios.post("http://localhost:3001/api/userData", {
+                url
+            })
+            console.log(response)
+        }catch(error){
+            console.log(error)
+        }
+    }
+    
+
     return(
         <>
+        <div>
+            <h1>Enter site url</h1>
+            <input type="text" placeholder="Enter site name"  value={url} onChange={(e) => setUrl(e.target.value)}/>
+            <button onClick={datasend}>submit</button>
+
+            <h2>elements to be scraped</h2>
+            <input type="text" placeholder="Enter elements" />
+            
+        </div>
         </>
     )
 }
