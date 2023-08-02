b54be78345cd4d71da965fe02dfe5b18acb97ce9
Udhay rajeev <udhay1co.de@gmail.com>
2023-08-02 20:37:39 +0530

    dahshboard fixing

--- pages/user/dashboard.tsx
+++ pages/user/dashboard.tsx
@@ -24,6 +24,22 @@ export default function dashboard(){
         <h1 className="ml-[2.5rem]  text-2xl font-semibold">Welcome, <span className="underline underline-offset-2">Udhay</span></h1>
         </div>
 
+        {/* Scrapers */}
+        <div className="bg-gray-700 bg-opacity-80 p-4 w-[40.5rem] h-[15rem] ml-[2.5rem] mt-[2.5rem] rounded-md">
+          <h2 className="text-2xl font-semibold ml-[2.5rem]">Your Scrapers</h2>
+          {/* parent scraper */}
+          <div className="flex">
+            <div className="w-1/2 bg-blue-300 p-4 mr-[4rem]">Scraper -1</div>
+            <div className="w-1/2 bg-green-300 p-4">Scraper -2</div>
+            <div className="w-1/2 bg-yellow-500">scraper - 3</div>
+
+          </div>
+
+        
+
+
+        </div>
+                
 
         
 
