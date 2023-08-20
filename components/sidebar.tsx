c9cf016f7e335acc12b8f0c88e29119fe5a25ece
Udhay rajeev <udhay1co.de@gmail.com>
2023-08-20 11:19:15 +0530

    navbar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,33 @@
 import Image from "next/image"
-import pluse from "../public/plus.png"
+import notifications from "../public/notifsvg.svg"
+import pluse from "../public/plus.svg"
+import analytics from "../public/analytics.svg"
+import documentation from "../public/doc.svg"
+import exports from "../public/export.svg"
+import logo from "../public/logo.svg"
+import logout from "../public/logout.svg"
 const SideBar = () => {
     return(
+        <body>
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-[#09090A] border border-[#1F1F22] shadow-lg">
+            {/* Main logo */}
             <div>
-                <span className="sidebar-icon">
+            <span className="sidebar-icon ">
                     <Image 
+                
+                        src={logo}
+                        alt="pluse"
+                        width={25}
+                        height={24}
+                    />
+                    
+                </span>
+
+            </div>
+            <div className="flex flex-col space-y-4">
+                <span className="sidebar-icon ">
+                    <Image 
+                
                         src={pluse}
                         alt="pluse"
                         width={25}
@@ -13,9 +35,73 @@ const SideBar = () => {
                     />
                     
                 </span>
+
+                <span className="sidebar-icon ">
+                    <Image 
+                
+                        src={analytics}
+                        alt="pluse"
+                        width={25}
+                        height={24}
+                    />
+                    
+                </span>
+
+                <span className="sidebar-icon ">
+                    <Image 
+                
+                        src={notifications}
+                        alt="pluse"
+                        width={25}
+                        height={24}
+                    />
+                    
+                </span>
+
+                <span className="sidebar-icon ">
+                    <Image 
+                
+                        src={documentation}
+                        alt="pluse"
+                        width={25}
+                        height={24}
+                    />
+                    
+                </span>
+
+                <span className="sidebar-icon ">
+                    <Image 
+                
+                        src={exports}
+                        alt="pluse"
+                        width={25}
+                        height={24}
+                    />
+                    
+                </span>
+                
+            </div>
+
+            {/* logout.svg */}
+            <div className="my-[20rem]">
+                <span>
+                <span className="sidebar-icon ">
+                    <Image 
+                
+                        src={logout}
+                        alt="pluse"
+                        width={25}
+                        height={24}
+                    />
+                    
+                </span>
+                </span>
             </div>
         
         </div>
+
+
+        </body>
     )
 
 }
