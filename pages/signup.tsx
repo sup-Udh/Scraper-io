54fe43531d9ec95e0af92d4be142581ab9b17ee3
Udhay rajeev <udhay1co.de@gmail.com>
2023-09-04 16:57:48 +0530

    singup.tsx changes

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,8 +2,8 @@ export default function singup(){
 
     return(
         <>
-        <div className="flex justify-center mt-[10rem]">
-        <div className="mr-[15.25rem] ">
+        <div className="flex justify-center mt-[10rem] font-[Poppins]">
+        <div className="mr-[16.25rem] ">
         <h1 className="text-xl font-bold">Sign Up</h1>
         </div>
         <br />
@@ -13,26 +13,30 @@ export default function singup(){
 
         </div>
 
-        <div className="mt-[2.25rem] flex justify-center mr-[2rem]">
+        <div className="mt-[2.25rem] flex justify-center mr-[2rem] font-[Poppins]">
             <form>
                 <p className="font-light text-[16px]">Name</p>
-                <input className="mt-[0.25rem] border border-sky-500 rounded-md w-[300px] h-[36px]" placeholder="naruto" />
+                <input className="mt-[0.25rem] border-2 border-[#3C3E43] placeholder-gray-500 placeholder:pl-[0.25rem] text-normal font-[Poppins] rounded-md w-[300px] h-[36px] mt-[0.35rem] bg-black" placeholder="naruto" />
 
 
-                <p className="font-light text-[16px]">Name</p>
-                <input className="mt-[0.25rem] border border-sky-500 rounded-md w-[300px] h-[36px]" placeholder="naruto" />
+                <p className="font-light text-[16px] mt-[0.25rem]">Email</p>
+                <input className="mt-[0.25rem] bg-black border-2 border-[#3C3E43] placeholder-gray-500 placeholder:pl-[0.25rem] text-normal rounded-md w-[300px] h-[36px] mt-[0.35rem]" placeholder="naruto@gmail.com" />
 
-                <p className="font-light text-[16px]">Name</p>
-                <input className="mt-[0.25rem] border border-sky-500 rounded-md w-[300px] h-[36px]" placeholder="naruto" />
-
-
-                <p className="font-light text-[16px]">Name</p>
-                <input className="mt-[0.25rem] border border-sky-500 rounded-md w-[300px] h-[36px]" placeholder="naruto" />
+                <p className="font-light text-[16px] mt-[0.25rem]">Password</p>
+                <input className="mt-[0.25rem] border-2 border-[#3C3E43] placeholder-gray-200 placeholder:pl-[0.25rem] bg-black text-normal rounded-md w-[300px] h-[36px] mt-[0.35rem]" placeholder="******" />
 
 
-                <p className="font-light text-[16px]">Name</p>
-                <input className="mt-[0.25rem] border border-sky-500 rounded-md w-[300px] h-[36px]" placeholder="naruto" />
+                <p className="font-light text-[16px] mt-[0.25rem]">Confirm Password</p>
+                <input type="password" className="mt-[0.25rem] border-2 border-[#3C3E43] placeholder-gray-200 placeholder:pl-[0.25rem] text-normal bg-black rounded-md w-[300px] h-[36px] mt-[0.35rem]" placeholder="******" />
 
+                <br />
+                <a>
+                    <div className="mt-[2.35rem] w-[300px] h-[38px] flex justify-center bg-[#7F61F4] rounded-lg">
+                                    <button className="text-white font-normal text-base ">Singup</button>
+
+                </div>
+
+                </a>
                 
             </form>
         </div>
