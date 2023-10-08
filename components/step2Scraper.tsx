499b1b97c9b0ce8791a4483bd87f829a1efc4cc9
Udhay rajeev <udhay1co.de@gmail.com>
2023-10-08 10:48:51 +0530

    state llogic hooks

--- components/step2Scraper.tsx
+++ components/step2Scraper.tsx
@@ -1,6 +1,6 @@
 // keep this element hidden until the user clicks the button to scrape the data
 
-export default function step2Scraper(){
+export default function Step2Scraper(){
     return(
         <h1>hi</h1>
     )
--- pages/user/newscraper.tsx
+++ pages/user/newscraper.tsx
@@ -1,24 +1,40 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 import axios from 'axios';
 import  Sidebar  from "@/components/SideBar"
-import step2Scraper from '@/components/step2Scraper';
+import Step2Scraper from '@/components/step2Scraper';
 
 export default function newscraper(){
-    const [showMe, setShowMe] = useState(false);
-    function toggle(){
-        setShowMe(!showMe);
+    const [currentDiv, setCurrentDiv] = useState(0);
+
+    function showNextDiv() {
+      setCurrentDiv(currentDiv + 1);
+    }
+  
+    function showPrevDiv() {
+      setCurrentDiv(currentDiv - 1);
     }
  
 
     return(
         <>
+        {/* <div style={{ display: currentDiv === 0 ? 'block' : 'none' }}>This is div 1.</div>
+      <div style={{ display: currentDiv === 1 ? 'block' : 'none' }}>This is div 2.</div>
+      <div style={{ display: currentDiv === 2 ? 'block' : 'none' }}>This is div 3.</div>
+      <div style={{ display: currentDiv === 3 ? 'block' : 'none' }}>This is div 4.</div>
+      <div style={{ display: currentDiv === 4 ? 'block' : 'none' }}>This is div 5.</div>
+      <button onClick={showPrevDiv} disabled={currentDiv === 0}>
+        Show previous div
+      </button>
+      <button onClick={showNextDiv} disabled={currentDiv === 4}>
+        Show next div
+      </button> */}
 
         <div id="ParentDiv" className='pl-[5rem] ' >
             {/* Sidebar */}
             <Sidebar />
 
 
-            <div className='pt-[2.25rem] ' style= {{display: showMe?"block":"none"}}>
+            <div className='pt-[2.25rem] ' >
             <h1 className='text-[30px] font-medium '>Setup Your Scraper</h1>
             </div>
 
@@ -26,6 +42,7 @@ export default function newscraper(){
             <div className='mt-[2.25rem] bg-[#111827] w-[1281px] h-[498px] rounded-md'>
 
                 {/* steps */}
+                <div className='Main-1' style={{display: currentDiv === 0 ? 'block' : 'none'}}>
                 <span className='pl-[1rem]  text-gray-200 text-[15px]  '>
                     <div className='pl-[2.25rem] font-medium text-gray-200 text-[15px]'>
                     Step 1 of 5
@@ -58,9 +75,21 @@ export default function newscraper(){
                         <p className=' ml-[2.25rem] mt-[0.25rem] font-light text-gray-400 text-[12px]  '>(you can edit this later on.)</p>
                     </div>
                     <br />
+                    </div>
+                    {/* 2nd div */}
+                    <div>
+                    <div className='Main-2' style={{display: currentDiv === 1 ? 'block' : 'none'}}>
+                        <Step2Scraper />
+                        
+                    </div>
+                    </div>
+                    {/* 3rd div */}
                     <span className='text-white font-medium text-[18px]'>
                         <div className='pl-[2.25rem] mt-[0.25rem]'>
-                        <button onClick={toggle} className='bg-[#7F61F4] h-[38px] w-[116px] rounded-md shadow-md hover:bg-[#7F61F4]/90'>Next</button>
+                        <button onClick={showNextDiv} className='bg-[#7F61F4] h-[38px] w-[116px] rounded-md shadow-md hover:bg-[#7F61F4]/90'>Next</button>
+                        <button onClick={showPrevDiv} hidden={currentDiv === 0}>
+        Show previous div
+      </button>
 
                         </div>
                     </span>
